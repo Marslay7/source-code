cdf58f63aba4321bffba5559c14bfefa654138ba
Marslay7 <331412466@qq.com>
2020-02-03 15:14:12 +0800

    source-code 20200203

--- src/compiler/index.js
+++ src/compiler/index.js
@@ -1,25 +1,29 @@
 /* @flow */
 
-import { parse } from './parser/index'
-import { optimize } from './optimizer'
-import { generate } from './codegen/index'
-import { createCompilerCreator } from './create-compiler'
+import { parse } from "./parser/index";
+import { optimize } from "./optimizer";
+import { generate } from "./codegen/index";
+import { createCompilerCreator } from "./create-compiler";
 
 // `createCompilerCreator` allows creating compilers that use alternative
 // parser/optimizer/codegen, e.g the SSR optimizing compiler.
 // Here we just export a default compiler using the default parts.
-export const createCompiler = createCompilerCreator(function baseCompile (
+export const createCompiler = createCompilerCreator(function baseCompile(
   template: string,
   options: CompilerOptions
 ): CompiledResult {
-  const ast = parse(template.trim(), options)
+  // 1.解析：将模版转换为对象AST(抽象语法树)
+  const ast = parse(template.trim(), options);
+
+  // 2.优化：标记静态节点，diff时可以直接跳过
   if (options.optimize !== false) {
-    optimize(ast, options)
+    optimize(ast, options);
   }
-  const code = generate(ast, options)
+  // 3.代码生成：转换ast为代码字符串 new Function(code)
+  const code = generate(ast, options);
   return {
     ast,
     render: code.render,
     staticRenderFns: code.staticRenderFns
-  }
-})
+  };
+});
--- src/core/vdom/patch.js
+++ src/core/vdom/patch.js
@@ -143,10 +143,12 @@ export function createPatchFunction(backend) {
     }
 
     vnode.isRootInsert = !nested; // for transition enter check
+    // 如果要创建是组件，走下面的流程
     if (createComponent(vnode, insertedVnodeQueue, parentElm, refElm)) {
       return;
     }
 
+    // 原生标签的创建
     const data = vnode.data;
     const children = vnode.children;
     const tag = vnode.tag;
@@ -211,10 +213,13 @@ export function createPatchFunction(backend) {
     }
   }
 
+  // 这里的createComponent是把前面的那个执行的结果vnode转换为真实dom
   function createComponent(vnode, insertedVnodeQueue, parentElm, refElm) {
+    // 获取管理钩子函数
     let i = vnode.data;
     if (isDef(i)) {
       const isReactivated = isDef(vnode.componentInstance) && i.keepAlive;
+      // 如果存在init钩子，则执行之创建实例并挂载
       if (isDef((i = i.hook)) && isDef((i = i.init))) {
         i(vnode, false /* hydrating */);
       }
@@ -222,8 +227,11 @@ export function createPatchFunction(backend) {
       // it should've created a child instance and mounted it. the child
       // component also has set the placeholder vnode's elm.
       // in that case we can just return the element and be done.
+      // 如果组件实例存在
       if (isDef(vnode.componentInstance)) {
+        // 属性初始化
         initComponent(vnode, insertedVnodeQueue);
+        // dom插入
         insert(parentElm, vnode.elm, refElm);
         if (isTrue(isReactivated)) {
           reactivateComponent(vnode, insertedVnodeQueue, parentElm, refElm);
@@ -665,6 +673,7 @@ export function createPatchFunction(backend) {
     // note we only do this if the vnode is cloned -
     // if the new node is not cloned it means the render functions have been
     // reset by the hot-reload-api and we need to do a proper re-render.
+    // 静态节点判断
     if (
       isTrue(vnode.isStatic) &&
       isTrue(oldVnode.isStatic) &&
